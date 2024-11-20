22c9f33df7d033c9314b3531b0e2090bbae9968c
Ashwani <ashwani@Ankits-MacBook.local>
2024-11-20 20:27:34 +0530

    route created sucessfully

--- server.js
+++ server.js
@@ -2,13 +2,7 @@ const express = require("express");
 const dotenv = require('dotenv');
 
 //Load env vars;
-
-// dev branch code 
-
 dotenv.config({ path: './config/config.env'});
-
-console.log("Dev brach created..")
-console.log("Dev branch deleted");
 
 const PORT = process.env.PORT || 5002;
 
@@ -21,3 +15,28 @@ app.listen(PORT,(err) => {
       console.log(`Server connected in ${process.env.NODE_ENV} mode on port ${PORT}`);
     }
   });
+
+app.get('/api/v1/bootcamps',(req,res) =>{
+  res.status(200).json({sucess:true,msg:'Show all bootcamps'})
+});
+
+app.get("/api/v1/bootcamps:id",(req,res) =>{
+    res.status(200).json({sucess:true,msg:`Fetch bootcamps with ${req.params.id}`})
+    })
+
+app.put("/api/v1/bootcamps:id",(req,res) =>{
+        res.status(200).json({sucess:true,msg:`Updated bootcamps with ${req.params.id}`})
+        })
+        
+app.delete("/api/v1/bootcamps:id",(req,res) => {
+            res.status(200).json({sucess:true,msg:`Delete bootcamps with ${req.params.id}`})
+        })
+
+app.post("/api/v1/bootcamps",(req,res) =>{
+    res.status(200).json({sucess:true,msg:'Create new bootcamps'})
+})
+
+
+
+
+
